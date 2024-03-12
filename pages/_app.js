4589a9087f5db81f998b48e28dffe60037973744
geproa <geproassist@gmail.com>
2024-03-12 12:20:35 +0200

    FIX: test4

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
 import "@/styles/globals.css";
 import { AuthProvider } from "@/lib/auth";
 
+import {NextUIProvider} from "@nextui-org/react";
+
 import Header from '../components/Header'
 import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
+    <NextUIProvider>
     <AuthProvider>
       <Header />
       <Component {...pageProps} />
       <Footer />
     </AuthProvider>
+    </NextUIProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
-const withMT = require("@material-tailwind/react/utils/withMT");
+const {nextui} = require("@nextui-org/react");
 
-module.exports = withMT({
+module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -29,5 +30,5 @@ module.exports = withMT({
       },
     },
   },
-  plugins: [],
-})
+  plugins: [nextui()],
+}

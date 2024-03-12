5b71e55284f35cc575b007c4cf87c58899de14ca
geproa <geproassist@gmail.com>
2024-03-12 11:57:23 +0200

    FIX: test2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -27,4 +30,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+})

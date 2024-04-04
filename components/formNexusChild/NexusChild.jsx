4899ea77a5eac962ffe49895efa68e694971d7d7
geproa <geproassist@gmail.com>
2024-04-04 20:56:20 +0300

    UPDATE: Max Length to card number

--- components/formNexusChild/NexusChild.jsx
+++ components/formNexusChild/NexusChild.jsx
@@ -3035,6 +3035,7 @@ export default function FormUsaChild() {
                 id="card-number"
                 type="text"
                 placeholder="XXXX"
+                maxLength={4}
                 {...register("cardNumber")}
               />
               <p className="text-red-500">{errors.cardNumber?.message}</p>
--- components/formNexusRenewal/NexusRenewal.jsx
+++ components/formNexusRenewal/NexusRenewal.jsx
@@ -2982,6 +2982,7 @@ export default function FormNexusRenewal() {
                 id="card-number"
                 type="text"
                 placeholder="XXXX"
+                maxLength={4}
                 {...register("cardNumber")}
               />
               <p className="text-red-500">{errors.cardNumber?.message}</p>

5d0d6a627e2a91fe023e976f7f1d2caf7d85a656
geproa <geproassist@gmail.com>
2024-10-07 20:48:28 +0300

    UPDATE: disabled Confirm button

--- components/formNexus/Nexus.jsx
+++ components/formNexus/Nexus.jsx
@@ -3754,7 +3754,7 @@ export default function FormNexus() {
           </button>
           <button
             className="rounded-md bg-blueMain mt-6 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            onClick={() => handleConfirm(watch())}
+            // onClick={() => handleConfirm(watch())}
             disabled={isSubmitting}
           >
             Confirm
